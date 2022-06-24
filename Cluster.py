1af3db2416a316fc38381f85f05acdec28aef9b1
LIDORZ55 <lidorzurzur@gmail.com>
2022-06-24 22:55:45 +0300

    24/6-finished-nomap

--- Cluster.py
+++ Cluster.py
@@ -1,6 +1,93 @@
+import pandas as pd
+import numpy as np
+
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import chart_studio.plotly as py
+import plotly.graph_objects as go
+
+
 class Cluster:
+
     def preprocess(self, file_path):
+        #reading the file
+        df = pd.read_excel(file_path.get())
+        #handling missing values inside our dataframe
+        #first we will see where the missing values are:
+        series = df.apply(lambda x: sum(x.isnull()),axis=0) #series of column name and num of missing values
+        imputer = SimpleImputer(missing_values=np.NaN, strategy='mean')
+        for i in series.index: #for an item in that series we will check if it has NaN values and if it does we will fill them with the mean
+            if (series[i] > 0):
+                df[i] = imputer.fit_transform(df[i].values.reshape(-1, 1))[:, 0]
+        #after we take care of the missing values we will normalize our data using sklearn preprocessing
+        scaler = StandardScaler()
+        d = df.columns.drop('country')
+        df[d] = scaler.fit_transform(df[d])
+        #now we will group by the country
+        df = df.groupby(['country']).mean()
+        #we will drop the year column
+        df.drop('year', inplace=True, axis=1)
+        return df
+
+
+    def cluster(self, dataframe, k, runs):
+        #create the KMeans model
+        kmeans = KMeans(n_clusters=k,n_init=runs).fit(dataframe)
+        #adding the output for each country to the dataframe
+        vals = []
+        for i in range(len(dataframe)):
+            newarr = dataframe.iloc[i, :].values
+            vals.append(kmeans.predict(newarr.reshape(1, -1))[0])
+        dataframe['algorithmOutput'] = vals
+        #create figure 1:
+        Y = dataframe['Generosity']
+        X = dataframe['Social support']
+        colors = dataframe['algorithmOutput']
+        plt.scatter(X, Y, c=colors, cmap='viridis')
+
+        dsada = plt.colorbar()
+        dsada.set_label('Cluster Number to color')
+
+        plt.title('Generosity-Social Support and their corresponding cluster')
+        plt.xlabel('Social support')
+        plt.ylabel('Generosity')
+
+
+        plt.savefig('plot1.png',dpi=82)
+        plt.close()
+
+        #make the countries be a columns insted of the index
+        #dataframe_temp = dataframe.reset_index(level=0)
+
+        #histograma - we need the 3 letter code for the countries to use it:
+        # py.sign_in("lidor12","NblOUQlm9HPqrNKAgPUw")
+        # fig = go.Figure(data=go.Choropleth(
+        #     locations=dataframe_temp['country'],
+        #     z=dataframe_temp['algorithmOutput'],
+        #     text=dataframe_temp['country'],
+        #     colorscale='Blues',
+        #     autocolorscale=False,
+        #     reversescale=True,
+        #     marker_line_color='darkgray',
+        #     marker_line_width=0.5,
+        #     colorbar_title='Clusters',
+        # ))
+        # fig.update_layout(
+        #     title_text='World countries and the clusters they belong',
+        #     geo=dict(
+        #         showframe=False,
+        #         showcoastlines=False,
+        #         projection_type='equirectangular'
+        #     )
+        # )
+        # py.image.save_as(fig,filename='map.png')
         return
 
-    def cluster(self, file_path, k, runs):
-        return
+
--- GUI.py
+++ GUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
+from tkinter import PhotoImage
 from Cluster import Cluster
 import pandas as pd
 
@@ -12,50 +13,62 @@ class GUI:
     def __init__(self, master_window):
         self.master_window = master_window
         master_window.title("K Means Clustering")
-        self.master_window.geometry("850x400")
+        self.master_window.geometry("1300x600")
         self.k = -1
         self.runs = 0
         self.path_ok = False
         self.Clustermodel = Cluster()
+        self.preprocessingDone = False
+        self.currentDataframe = ""
+        self.fig1 = ""
+        self.fig2 = ""
+        self.img1 = ""
+        self.IsClustered = False
 
         # Path
         self.path_label = tk.Label(master_window, text="Data Path")
-        self.path_label.grid(row=0, column=0)
+        self.path_label.grid(row=0, column=0,sticky='W')
         self.file_Path = tk.StringVar()
-        ipv = self.master_window.register(self.IsPathValid)
-        self.path = tk.Entry(master_window, textvariable=self.file_Path, validate="key",
-                             validatecommand=(ipv, '%P'))
-        self.path.grid(row=0, column=1)
+        self.path = tk.Entry(self.master_window, textvariable=self.file_Path,state='disabled',bd=2,width=60)
+        self.path.grid(row=0, column=1,sticky='we')
         self.browse_button = tk.Button(master_window, text="Browse", command=self.getFilePath)
         self.browse_button.grid(row=0, column=2)
 
         # Number of clusters k
-        self.k_label = tk.Label(master_window, text="Number of clusters k")
+        self.k_label = tk.Label(self.master_window, text="Number of clusters k")
         self.k_label.grid(row=1, column=0)
         ikv = self.master_window.register(self.IsKValid)
-        self.k = tk.Entry(master_window, validate="key", validatecommand=(ikv, '%P'))
-        self.k.grid(row=1, column=1)
+        self.k = tk.Entry(self.master_window, validate="key", validatecommand=(ikv, '%P'))
+        self.k.grid(row=1, column=1,sticky='W')
 
         # Number of runs
-        self.runs_label = tk.Label(master_window, text="Number of runs")
-        self.runs_label.grid(row=2, column=0)
+        self.runs_label = tk.Label(self.master_window, text="Number of runs")
+        self.runs_label.grid(row=2, column=0,sticky='W')
         irv = self.master_window.register(self.IsRunsValid)
-        self.runs = tk.Entry(master_window, validate="key", validatecommand=(irv, '%P'))
-        self.runs.grid(row=2, column=1)
+        self.runs = tk.Entry(self.master_window, validate="key", validatecommand=(irv, '%P'))
+        self.runs.grid(row=2, column=1,sticky='W')
 
         # Pre_process
         self.pre_process_button = tk.Button(master_window, text="Pre-process", command=self.preprocess)
-        self.pre_process_button.grid(row=4, column=1)
+        self.pre_process_button.grid(row=4, column=1,sticky='we')
         self.pre_process_button["state"] = "disabled"  # can"t press until all previous requirements are fine
-
+        #
         # Cluster
         self.cluster_button = tk.Button(master_window, text="Cluster", command=self.cluster)
-        self.cluster_button.grid(row=5, column=1)
+        self.cluster_button.grid(row=5, column=1,sticky='we')
         self.cluster_button["state"] = "disabled"  # can"t press until all previous requirements are fine
+
+        self.master_window.grid_rowconfigure(6,weight=1)
+        self.master_window.grid_columnconfigure(1, weight=1)
+        self.master_window.grid_columnconfigure(3, weight=1)
 
     # update file_path after picking one with the browser
     def getFilePath(self):
         try:
+            self.preprocessingDone = False
+            self.currentDataframe = ""
+            self.cluster_button["state"] = "disabled"
+            self.pre_process_button["state"] = "disabled"
             self.file_Path.set(filedialog.askopenfilename(title="Select Data file", initialdir="/",
                                                           filetypes=[('Excel files', '*.xlsx')]))
             # self.file_Path = filedialog.askopenfilename(title="Select Data file", initialdir="/",
@@ -70,71 +83,109 @@ class GUI:
             self.path_ok = False
             if self.pre_process_button["state"] == "active":
                 self.pre_process_button["state"] = "disabled"
-            if not path or path == "":
+            if path.get() == "":
                 return False
-            try:
-                pd.read_csv(path)
-                self.path_ok = True
-            except pd.errors.EmptyDataError:  # File is empty
-                tk.messagebox.showinfo(title="K Means Clustering", message=str(path) + " file is empty!")
-            if self.path_ok and isinstance(self.k, int) and 0 < self.k < 10 and isinstance(self.runs, int) and self.runs > 0:
-                self.pre_process_button["state"] = "active"
-        except ValueError:
+            df = pd.read_excel(path.get())
+            if df.empty:
+                tk.messagebox.showinfo(title="K Means Clustering", message=str(path.get()) + " is empty!")
+                self.file_Path.set("")
+                return False
+            self.path_ok = True
+            self.pre_process_button["state"] = "active"
+        except Exception as e:
+            print(e)
             tk.messagebox.showinfo(title="K Means Clustering", message="Bad parameters. Please try again")
 
     # Checks if number of clusters k is valid
-    def IsKValid(self, num_input):
+    def IsKValid(self, num_input): #n_clusters
         try:
-            if num_input == "":
-                self.k = ""  # User didn't fill it yet
+            if num_input.isdigit(): #if the user filled an integer
+                self.k = int(num_input)  # convert
+                if self.k <= 0 or self.k > 15:
+                    self.cluster_button["state"] = "disabled"
+                    messagebox.showinfo(title="K Means Clustering",message="Bad parameters,enter a number between 1 and 15")
+                    return False
+                if 0 < self.k <= 15:
+                    if isinstance(self.runs, int):
+                        if self.preprocessingDone and self.runs >= 10 and self.runs <= 300:
+                            self.cluster_button["state"] = "active"
+                        else:
+                            self.cluster_button["state"] = "disabled"
+                    else:
+                        self.cluster_button["state"] = "disabled"
+                else:
+                    self.cluster_button["state"] = "disabled"
                 return True
-            if not num_input:  # Check if there is any input from user
+            elif num_input == "": #if the user didnt fill it yet
+                self.k = ""  # User didn't fill it yet
+                self.cluster_button["state"] = "disabled"
+                return True
+            else: #if input is not good
+                self.cluster_button["state"] = "disabled"
+                messagebox.showinfo(title="K Means Clustering", message="Bad parameters,enter a number between 1 and 15")
                 return False
-            self.k = int(num_input)  # convert
-            if 0 < self.k < 10:
-                return
-            if self.path_ok and isinstance(self.k, int) and 0 < self.k < 10 and isinstance(self.runs, int) and self.runs > 0:
-                self.pre_process_button["state"] = "active"
         except ValueError:
-            messagebox.showinfo(title="K Means Clustering", message="Bad parameters")
+            messagebox.showinfo(title="K Means Clustering", message="Bad parameters,enter a number between 1 and 15")
 
     # Checks if number of runs is valid
-    def IsRunsValid(self, num_input):
+    def IsRunsValid(self, num_input): #n_init
         try:
-            self.path_ok = False
-            if num_input == "":
-                self.runs = ""  # User didn't fill it yet
+            if num_input.isdigit():  # if the user filled an integer
+                self.runs = int(num_input)  # convert
+                if self.runs <= 0 or self.runs > 300:
+                    self.cluster_button["state"] = "disabled"
+                    messagebox.showinfo(title="K Means Clustering",message="Error, enter a number between 10 and 300")
+                    return False
+                # if self.path_ok and 0 < self.k <= 15 and isinstance(self.runs,int) and self.runs > 0: probably go back
+                if 10 <= self.runs <= 300:
+                    if isinstance(self.k, int):
+                        if self.preprocessingDone and self.k > 0 and self.k <= 15:
+                            self.cluster_button["state"] = "active"
+                        else:
+                            self.cluster_button["state"] = "disabled"
+                    else:
+                        self.cluster_button["state"] = "disabled"
+                else:
+                    self.cluster_button["state"] = "disabled"
                 return True
-            if not num_input:  # Check if there is any input from user
+            elif num_input == "":  # if the user didnt fill it yet
+                self.runs = ""  # User didn't fill it yet
+                self.cluster_button["state"] = "disabled"
+                return True
+            else:  # if input is not good
+                self.cluster_button["state"] = "disabled"
+                messagebox.showinfo(title="K Means Clustering",message="Error,enter a number between 10 and 300")
                 return False
-            self.runs = int(num_input)  # convert
-            if self.runs > 0:
-                return num_input
-            if self.path_ok and isinstance(self.k, int) and 0 < self.k < 10 and isinstance(self.runs, int) and self.runs > 0:
-                self.pre_process_button["state"] = "active"
         except ValueError:
-            messagebox.showinfo(title="K Means Clustering", message="Bad parameters")
+            messagebox.showinfo(title="K Means Clustering", message="Error,enter a number between 10 and 300")
 
-    # Load the file, process it and clean it
+    #process the file and clean it
     def preprocess(self):
         try:
-            if self.k != "" and 0 < self.k < 10 and self.runs > 0:
-                self.Clustermodel.preprocess(self.file_Path)  # Call the pre-process method
-                tk.messagebox.showinfo(title="Pre-Process complete", message="Data was pre-processed")
-                self.cluster_button["state"] = "active"
-            else:
-                tk.messagebox.showinfo(title="Pre-Process error", message="Bad parameters. Please try again")
-        except ValueError:
-            tk.messagebox.showinfo(title="Pre-Process error", message="Bad parameters. Please try again")
+            self.currentDataframe = self.Clustermodel.preprocess(self.file_Path)  # Call the pre-process method
+            #print(self.currentDataframe)
+            tk.messagebox.showinfo(title="K Means Clustering", message="Preprocessing completed successfully!")
+            self.preprocessingDone=True
+            if isinstance(self.runs, int) and isinstance(self.k, int):
+                if self.preprocessingDone and self.runs >= 10 and self.runs <= 300 and self.k > 0 and self.k <= 15:
+                    self.cluster_button["state"] = "active"
+        except Exception as e:
+            tk.messagebox.showinfo(title="K Means Clustering", message="Preprocessing couldn't complete")
 
     # Build K-Means model and make visualization output
     def cluster(self):
         try:
-            self.Clustermodel.cluster(self.file_Path, self.k, self.runs)  # Call the cluster method
-            tk.messagebox.showinfo(title="Clusters complete", message="K-means model and clusters visualization done")
-            self.master_window.destroy()
+            self.Clustermodel.cluster(self.currentDataframe, self.k, self.runs)  # Call the cluster method
+            self.img1 = PhotoImage(file='plot1.png')
+            self.fig1 = tk.Label(self.master_window,image=self.img1)
+            self.fig1.grid(row=6, column=3,sticky='wens')
+            self.fig2 = tk.Label(self.master_window, image=self.img1)
+            self.fig2.grid(row=6, column=1, sticky='wens')
+            self.IsClustered = True
+            tk.messagebox.showinfo(title="K Means Clustering", message="K-means model and clusters visualization done")
+            #self.master_window.destroy()
         except ValueError:
-            tk.messagebox.showinfo(title="Clusters error", message="Bad parameters. Please try again")
+            tk.messagebox.showinfo(title="K Means Clustering", message="Bad parameters. Please try again")
 
 
 root = tk.Tk()
